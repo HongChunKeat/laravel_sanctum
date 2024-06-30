64722391fe49dd1878afd058bfc2da09a49fe5ca
Eric Hong <erichong8442@gmail.com>
2024-07-01 03:32:29 +0800

    fix

--- app/Cronjob/ExpiredToken.php
+++ app/Cronjob/ExpiredToken.php
@@ -3,11 +3,13 @@
 namespace App\Cronjob;
 
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 
 class ExpiredToken
 {
     public function __invoke()
     {
         DB::table("sw_personal_access_tokens")->where("expires_at", "<=", date("Y-m-d H:i:s"))->delete();
+        Log::info(date("Y-m-d H:i:s"));
     }
 }
--- routes/console.php
+++ routes/console.php
@@ -6,5 +6,6 @@ use Illuminate\Support\Facades\Schedule;
 use App\Cronjob\ExpiredToken;
 
 # https://laravel.com/docs/11.x/scheduling
+// crontab -e then put php /path-to-your-project/artisan schedule:run >> /dev/null 2>&1
 
 Schedule::call(new ExpiredToken)->cron("* * * * *");
